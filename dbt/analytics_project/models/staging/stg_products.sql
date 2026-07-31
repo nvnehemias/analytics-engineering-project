e24e88c7dc5b524dd276fded0496bec1d1bb7b67
Nehemias Vicente <nvnehemias@gmail.com>
2026-07-31 13:08:28 -0700

    Updating product table column names

--- dbt/analytics_project/models/staging/stg_products.sql
+++ dbt/analytics_project/models/staging/stg_products.sql
@@ -5,5 +5,12 @@ with products as (
 )
 
 select 
-*
+    product_id
+    , product_category_name as category_name
+    , product_photo_qty as photo_qty 
+    , product_weight_g as weight_g
+    , product_length_cm as length_cm
+    , product_height_cm as height_cm 
+    , product_width_cm as width_cm
+    
 from products
