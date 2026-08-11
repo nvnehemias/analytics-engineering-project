b1c6f557b35a3b48acc6485ca5e95dcd3750c2b1
Nehemias Vicente <nvnehemias@gmail.com>
2026-08-11 13:21:30 -0700

    Adding comma

--- dbt/analytics_project/models/marts/core/dim_customers.sql
+++ dbt/analytics_project/models/marts/core/dim_customers.sql
@@ -14,7 +14,7 @@ with customers as (
         customer_id
         , min(order_purchase_timestamp) as first_order_date
         , max(order_purchase_timestamp) as last_order_date
-        count(order_id) as total_orders
+        , count(order_id) as total_orders
 
     from orders
     group by 1
@@ -29,6 +29,6 @@ select
     , co.first_order_date
     , co.last_order_date
     , coalesce(co.total_orders,0) as total_orders
-    
+
 from customers c 
 left join customer_orders co on c.customer_id = co.customer_id
