71e10a8196250391c67b303c70e6a2e94afbc6c8
Nehemias Vicente <nvnehemias@gmail.com>
2026-08-12 15:48:53 -0700

    Adding sql logic for dim_sellers

--- dbt/analytics_project/models/marts/core/dim_sellers.sql
+++ dbt/analytics_project/models/marts/core/dim_sellers.sql
@@ -4,7 +4,19 @@ with sellers as (
 
 )
 
+, geolocation as (
+
+    select * from {{ ref('int_geolocation_by_zip') }}
+
+)
 
 select 
-*
-from sellers
+    s.seller_id 
+    , s.zip_code as zip_code_prefix 
+    , s.city 
+    , s.state
+    , g.latitude
+    , g.longitude
+
+from sellers s 
+left join geolocation g on s.zip_code = g.geolocation_zip_code_prefix
